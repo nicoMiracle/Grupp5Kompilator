04d7a06533e59f9595c8cd8fd92f03203cb2580f
Developer Pavel <pavelpoldiaev@gmail.com>
2023-10-12 20:01:55 +0200

    latest version of project

---
+++ Kompilator/src/main/java/org/example/JavaLexer.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class JavaLexer {
+    private TokenStream tokenStream;
+    public JavaLexer(){
+        tokenStream=new TokenStream();
+    }
+    public void splitIntoTokens(){
+
+    }
+}
