07c25528e524159272951342484012cf35725e81
Developer Pavel <pavelpoldiaev@gmail.com>
2023-10-12 12:50:23 +0200

    giving this a more of a project look

---
+++ Kompilator/src/main/java/org/example/Token.java
@@ -0,0 +1,30 @@
+package org.example;
+
+public class Token {
+    private TokenType type; // type of the token
+    private String lexeme; // The value of token
+    private int line; // The line number where the token was found
+
+    public Token(TokenType type, String lexeme, int line) {
+        this.type = type;
+        this.lexeme = lexeme;
+        this.line = line;
+    }
+
+    public TokenType getType() {
+        return type;
+    }
+
+    public String getLexeme() {
+        return lexeme;
+    }
+
+    public int getLine() {
+        return line;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Token(type=%s, lexeme='%s', line=%d)", type, lexeme, line);
+    }
+}
