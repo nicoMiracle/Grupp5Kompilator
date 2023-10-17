cfda333def0b465fa533c23118567eda5e8b7b44
nicoMiracle <144761798+nicoMiracle@users.noreply.github.com>
2023-10-17 12:27:27 +0200

    Add files via upload

--- src/main/java/org/example/Code_Generator.java
+++ src/main/java/org/example/Code_Generator.java
@@ -1,5 +1,20 @@
 package org.example;
 
-public class Code_Generator {
+import com.sun.source.tree.*;
 
+public class Code_Generator {
+    public void visit(StatementTree node){
+    }
+    public void visit(IdentifierTree node){
+
+    }
+    public void visit(ExpressionTree node){
+
+    }
+    public void visit(LiteralTree node){
+
+    }
+    public void visit(UnaryTree node){
+
+    }
 }
--- src/test/java/Code_Generator_Test.java
+++ src/test/java/Code_Generator_Test.java
@@ -1,3 +1,15 @@
-public class Code_Generator_Test {
+import org.junit.jupiter.api.*;
+import org.junit.platform.engine.support.hierarchical.Node;
 
+import java.util.TreeSet;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class Code_Generator_Test {
+    @Test
+    @DisplayName("Generate code in Python for x=32;")
+    void generateIntAssign() {
+
+    }
 }
