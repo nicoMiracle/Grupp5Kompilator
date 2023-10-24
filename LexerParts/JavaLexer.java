5fc625d4e496ba66b1e7b27be16e82ae3ceca09b
Pavel <43667438+PavelP97@users.noreply.github.com>
2023-10-24 19:16:09 +0200

    Add files via upload
    Updated Lexer, now has one more help method to lex method a bit smaller. Also added new tokentypes like OUT, SYSTEM, PRINTLN, NEXT_LINE

--- LexerParts/JavaLexer.java
+++ LexerParts/JavaLexer.java
@@ -16,34 +16,14 @@ public class JavaLexer {
         charTokenMap.put('=', TokenType.ASSIGN);
         charTokenMap.put('+', TokenType.PLUS);
         charTokenMap.put('-', TokenType.MINUS);
-        charTokenMap.put(';', TokenType.EOF);
+        charTokenMap.put(';', TokenType.SEMICOLON);
         charTokenMap.put(',', TokenType.COMMA);
         charTokenMap.put('.', TokenType.DOT);
     }
     public void setListener(DefaultLexerErrorListener listener) {
         this.listener = listener;
     }
-    private String getTextBetweenQuotation(String text, int i) {
-        int j = i + 1;
-        while (j < text.length() && text.charAt(j) != '"') {
-            j++;
-        }
-        return text.substring(i + 1, j).trim(); // Trim removes leading and trailing spaces.
-    }
-    private String getSequenceOfLetters(String text, int i) {
-        int j = i;
-        while (j < text.length() && (Character.isLetter(text.charAt(j)))) {
-            j++;
-        }
-        return text.substring(i, j);
-    }
-    private String getSequenceOfNumbers(String text, int i) {
-        int j = i;
-        while (j < text.length() && (Character.isDigit(text.charAt(j)))) {
-            j++;
-        }
-        return text.substring(i, j);
-    }
+
     public List<Token> lex(String input) {
         List<Token> tokenList = new ArrayList<>();
         int currentPosition = 0;
@@ -68,11 +48,11 @@ public class JavaLexer {
                 }
                 tokenList.add(new Token(tokenType, tokenValue, lineNumber));
                 currentPosition++;
-                if (tokenType==TokenType.EOF) {
+                if (tokenType==TokenType.SEMICOLON) {
                     lineNumber++;
                 }
             } else if (currentChar == '"') {
-                String stringValue = getTextBetweenQuotation(input, currentPosition);
+                String stringValue = getTextBetweenQuotationMarks(input, currentPosition);
                 tokenList.add(new Token(TokenType.STRING_LITERAL, stringValue, lineNumber));
                 currentPosition += stringValue.length() + 2; // Skip past the string and both double quotes
             } else if (Character.isDigit(currentChar)) {
@@ -82,19 +62,48 @@ public class JavaLexer {
             } else if (Character.isLetter(currentChar)) {
                 String letter = getSequenceOfLetters(input, currentPosition);
                 currentPosition += letter.length();
-                switch (letter) {
-                    case "int" -> tokenList.add(new Token(TokenType.TYPE_INT, letter, lineNumber));
-                    case "String" -> tokenList.add(new Token(TokenType.TYPE_STRING, letter, lineNumber));
-                    case "if" -> tokenList.add(new Token(TokenType.IF, letter, lineNumber));
-                    case "while" -> tokenList.add(new Token(TokenType.WHILE, letter, lineNumber));
-                    case "return" -> tokenList.add(new Token(TokenType.FUNCTION_RETURN, letter, lineNumber));
-                    default -> tokenList.add(new Token(TokenType.IDENTIFIER, letter, lineNumber));
-                }
+                handleIdentifierToken(tokenList, letter, lineNumber);
             } else {
                 listener.lexicalError("Unrecognized character: '" + currentChar + "'", lineNumber);
                 currentPosition++;
             }
         }
         return tokenList;
+    }
+    private String getTextBetweenQuotationMarks(String text, int i) {
+        int j = i + 1;
+        while (j < text.length() && text.charAt(j) != '"') {
+            j++;
+        }
+        return text.substring(i + 1, j).trim(); // Trim removes leading and trailing spaces.
+    }
+    private String getSequenceOfLetters(String text, int i) {
+        int j = i;
+        while (j < text.length() && (Character.isLetter(text.charAt(j)))) {
+            j++;
+        }
+        return text.substring(i, j);
+    }
+    private String getSequenceOfNumbers(String text, int i) {
+        int j = i;
+        while (j < text.length() && (Character.isDigit(text.charAt(j)))) {
+            j++;
+        }
+        return text.substring(i, j);
+    }
+    private void handleIdentifierToken(List<Token> tokenList, String letter, int lineNumber) {
+        switch (letter) {
+            case "System" -> tokenList.add(new Token(TokenType.SYSTEM, letter, lineNumber));
+            case "out" -> tokenList.add(new Token(TokenType.OUT, letter, lineNumber));
+            case "println" -> tokenList.add(new Token(TokenType.PRINTLN, letter, lineNumber));
+            case "input" -> tokenList.add(new Token(TokenType.INPUT, letter, lineNumber));
+            case "nextLine" -> tokenList.add(new Token(TokenType.NEXT_LINE, letter, lineNumber));
+            case "int" -> tokenList.add(new Token(TokenType.TYPE_INT, letter, lineNumber));
+            case "String" -> tokenList.add(new Token(TokenType.TYPE_STRING, letter, lineNumber));
+            case "if" -> tokenList.add(new Token(TokenType.IF, letter, lineNumber));
+            case "while" -> tokenList.add(new Token(TokenType.WHILE, letter, lineNumber));
+            case "return" -> tokenList.add(new Token(TokenType.FUNCTION_RETURN, letter, lineNumber));
+            default -> tokenList.add(new Token(TokenType.IDENTIFIER, letter, lineNumber));
+        }
     }
 }
